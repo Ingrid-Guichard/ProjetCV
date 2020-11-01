e746a0e6fbd18f8246ec7f87a3affb7d23dff483
Solene Charlet <solene.charlet@epfedu.fr>
2020-11-01 19:56:59 +0100

    test

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -1,8 +1,11 @@
 INSERT INTO defaultdb.users (id, first_name, last_name, age, address, job_status, mail, photo) VALUES (1, 'Elena', 'Ortega', 22, '46 rue mouchez', 'Etudiante', 'elena.ortega@epfedu.fr', 'elena.ortega.jpg');
 INSERT INTO defaultdb.languages(id, user_id, name, level) VALUES (1,1,'Espagnol','Bilingue');
-INSERT INTO defaultdb.educations(id, user_id, title, place, starting_date, final_date, summary) VALUES (1,1,'Stage 4A','NZ','2019-09-09','2019-12-20','Stage élève ingénieur de 4A');
+INSERT INTO defaultdb.skills(id, user_id, name) VALUES (1,1,'Java');
+INSERT INTO defaultdb.experiences(id, user_id, title, place, starting_date, final_date, summary) VALUES (1,1, 'Chef de projet', 'Takima', '2020-02-12', '2020-12-18', 'Gestion');
+INSERT INTO defaultdb.educations(id, user_id, title, place, starting_date, final_date, summary) VALUES (1,1,'Ingenieur','EPF','2016-06-18','2021-12-10','Master numerique');
+INSERT INTO defaultdb.projects(id, user_id, title, summary) VALUES (1,1, 'Projet 4A', 'Igny Vallee');
 INSERT INTO defaultdb.links(id, user_id, title, link, is_checked) VALUES (1,1, 'Facebook', null, false);
 INSERT INTO defaultdb.links(id, user_id, title, link, is_checked) VALUES (2,1, 'Instagram', null, false);
 INSERT INTO defaultdb.links(id, user_id, title, link, is_checked) VALUES (3,1, 'LinkedIn', null, false);
-INSERT INTO defaultdb.links(id, user_id, title, link, is_checked) VALUES (4,1, 'GitHub', null, false);
+INSERT INTO defaultdb.links(id, user_id, title, link, is_checked) VALUES (4,1, 'GitHub', 'https://gitlab.com/elena_ortega/projetcv/', true);
 INSERT INTO defaultdb.links(id, user_id, title, link, is_checked) VALUES (5,1, 'Twitter', null, false);
